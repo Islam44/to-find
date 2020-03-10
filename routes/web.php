14b2eb5c9a99377f2446518cbf73765d745e9098
Islam44 <islamemam95@yahoo.com>
2020-03-10 17:21:28 +0200

    add configuration settings

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 
 Auth::routes(['verify' => true]);
 Route::get('/', function () {
-    Artisan::call('queue:work', ['--queue' => 'high,default']);
+//    Artisan::call('queue:work', ['--queue' => 'high,default']);
     return view('welcome');
 })->middleware(increaseView::class);
 
